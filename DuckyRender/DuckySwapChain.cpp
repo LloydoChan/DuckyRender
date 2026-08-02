3f576ca280aaf35d34c7b264e1a20b46eb816ed6
Lloyd Crawley <lloyd.crawley@gmail.com>
2026-08-02 17:29:14 +0900

    made render targets SRGB

--- DuckyRender/DuckySwapChain.cpp
+++ DuckyRender/DuckySwapChain.cpp
@@ -45,6 +45,10 @@ bool DuckySwapChain::Init(D3DDeviceManager* DeviceManager, IDXGIFactory6* factor
 
 
 	D3D12_CPU_DESCRIPTOR_HANDLE handle = mRtvHeaps->GetCPUDescriptorHandleForHeapStart();
+	D3D12_RENDER_TARGET_VIEW_DESC srv{};
+	srv.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	srv.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+	srv.Texture2D.MipSlice = 0;
 
 	for (UINT idx = 0; idx < swapChainDesc.BufferCount; idx++)
 	{
@@ -54,7 +58,7 @@ bool DuckySwapChain::Init(D3DDeviceManager* DeviceManager, IDXGIFactory6* factor
 
 		if (FAILED(hResult) && !OutputErrorFromHResult(hResult, "problem getting back buffer: ", *LogFile)) return false;
 
-		device->CreateRenderTargetView(mBackBuffers[idx].Get(), nullptr, handle);
+		device->CreateRenderTargetView(mBackBuffers[idx].Get(), &srv, handle);
 		handle.ptr += device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 	}
 
